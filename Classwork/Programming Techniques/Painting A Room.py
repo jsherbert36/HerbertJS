3f64bee5e1b1968094b1ff2d7074d74b65475519
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-09-05 19:32:44 +0100

    homework 05/09/2019

---
+++ Classwork/Programming Techniques/Painting A Room.py
@@ -0,0 +1,16 @@
+unit = 'x'
+while unit == 'x':
+    print('Enter imperial or metric')
+    value = input()
+    if value == 'metric':
+        unit = 'metres'
+    elif value == 'imperial':
+        unit = 'feet'
+    else:
+        print('invalid input')    
+print('Enter the height, width and length of the room - in that order, in ',unit,'.')
+height, width, length = float(input()),float(input()),float(input())
+SA =((height*width*2) + (height*length*2) + (width * length)) 
+print('You need enough paint to cover ',SA,' ', unit, ' squared')
+
+
---
+++ Classwork/Programming Techniques/Rock Paper Scissors.py
@@ -0,0 +1,36 @@
+from random import randint
+from sys import exit
+Cscore,Uscore,userinput,choice = 0,0,0,['rock','paper','scissors','finish']
+def score():
+    print('SCORE:')
+    print('Computer:',Cscore,'Player:',Uscore,sep=' ')
+def result(WinOrLose):
+    print('The computer chose '+ choice[j] + ', you '+ WinOrLose + ' !')
+    score()
+while userinput != -1:
+    print('============================================================================')
+    print('NEW ROUND: Enter rock, paper or scissors. Enter finish to end the game.')
+    userinput = str(input())
+    j = randint(0,2)
+    if userinput in choice:
+        i = choice.index(userinput)
+        if i == j:
+            print('The computer also chose '+ choice[i] + ' so you draw!')
+            score()
+        elif (i == 0 and j == 1) or (i == 1 and j == 2) or (i == 2 and j == 0):
+            Cscore += 1
+            result('lose')
+        elif (i == 0 and j == 2) or (i == 1 and j == 0) or (i == 2 and j == 1):
+            Uscore += 1
+            result('win')
+        elif i == 3:
+            print('Thanks for playing!')
+            score()
+            userinput = -1
+    else:
+        print('invalid input')
+exit()
+        
+
+
+    
