c9d1b575af395ea367ead0fe40caf5a571cc5d68
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-09-14 17:53:25 +0100

    update

--- Mr Carroll Classwork/Programming Techniques/Times Tables.py
+++ Mr Carroll Classwork/Programming Techniques/Times Tables.py
@@ -1,15 +1,28 @@
-table,answer = -1,'N'
-while (table < 1 or table > 20):
-    print("Enter which times table you want from 1 to 20")
-    try:
-        table = int(input())
-    except typeerror:
-        print('Please enter an integer next time')
-    print('Enter how many rows you want:')
-    try:
-        rows = int(input())
-    except typeerror:
-        print('Please enter an integer next time')
-#endwhile
-print("You entered the ",table," for ",rows," rows"
-[print(i*int(table)) for i in range(int(rows))]
+table,answer = -1,'C'
+def entertable():
+    global table,Rows
+    while table != -1:
+        print("Enter which times table you want from 1 to 20")
+        table = input()
+        if table.isdigit() and rows not in range(1,21):
+            table = int(table)
+        else:
+            print('Try again next time!')
+        print('Enter how many rows you want:')
+        Rows = input()
+        if Rows.isdigit() and Rows not in range(1,31):
+            Rows = int(Rows)
+        else:
+            print('Try again next time!')
+            continue
+    #endwhile
+entertable()
+print("Did you enter the "+str(table)+" times table for "+str(Rows)+" rows")
+print("If yes, type 'Yes', if no, type 'No'")
+answer = input()
+if answer == 'Yes':
+    [print(i*table) for i in range(Rows)]
+    answer = 'X'
+else:
+    entertable()
+
