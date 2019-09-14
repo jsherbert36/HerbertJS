e3de79e9714c0b352ce84f0337059734b30729a1
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-09-14 15:42:57 +0100

    14/09/2019

--- Mr Carroll Classwork/Programming Techniques/Times Tables.py
+++ Mr Carroll Classwork/Programming Techniques/Times Tables.py
@@ -3,13 +3,13 @@ while (table < 1 or table > 20):
     print("Enter which times table you want from 1 to 20")
     try:
         table = int(input())
-    except typeerror:
+    except valueerror:
         print('Please enter an integer next time')
     print('Enter how many rows you want:')
     try:
         rows = int(input())
-    except typeerror:
+    except valueerror:
         print('Please enter an integer next time')
 #endwhile
-print("You entered the ",table," for ",rows," rows"
+print("You entered the ",table," for ",rows," rows")
 [print(i*int(table)) for i in range(int(rows))]
