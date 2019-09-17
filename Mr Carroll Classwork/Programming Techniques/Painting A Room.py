e779c9d8e5bf32b9c69b9d7edf528bfa76f65e57
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2019-09-17 21:38:15 +0100

    Feedback: Paint
    This is a good effort. Please update in line with the DC coding conventions

--- Mr Carroll Classwork/Programming Techniques/Painting A Room.py
+++ Mr Carroll Classwork/Programming Techniques/Painting A Room.py
@@ -7,9 +7,11 @@ while unit == 'x':
     elif value == 'imperial':
         unit = 'feet'
     else:
-        print('invalid input')    
+        print('invalid input')
+
+
 print('Enter the height, width and length of the room - in that order, in ',unit,'.')
-height, width, length = float(input()),float(input()),float(input())
+height, width, length = float(input()),float(input()),float(input()) ### SRC - This looks good, but please avoid multiple assignments
 SA =((height*width*2) + (height*length*2) + (width * length)) 
 print('You need enough paint to cover ',SA,' ', unit, ' squared.')
 
