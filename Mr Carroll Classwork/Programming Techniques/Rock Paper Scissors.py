df3297dfae16b1af0897bd16a67ba1f334fc7e95
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2019-09-17 21:47:01 +0100

    Feedback: Rock Paper Scissors
    This is a very promising start. I would try to start making my code look more like pseudocode, and please update according to the DC Coding conventions.

--- Mr Carroll Classwork/Programming Techniques/Rock Paper Scissors.py
+++ Mr Carroll Classwork/Programming Techniques/Rock Paper Scissors.py
@@ -1,17 +1,21 @@
 from random import randint
 from sys import exit
-Cscore,Uscore,userinput,choice = 0,0,0,['rock','paper','scissors','finish']
+Cscore,Uscore,userinput,choice = 0,0,0,['rock','paper','scissors','finish']  ### SRC - I really don't like multiple assignments!
 def score():
     print('SCORE:')
     print('Computer:',Cscore,'Player:',Uscore,sep=' ')
+### SRC - I'd like to see an end procedure here
+    
 def result(WinOrLose):
     print('The computer chose '+ choice[j] + ', you '+ WinOrLose + ' !')
     score()
+
+    
 while userinput != -1:
-    print('============================================================================')
+    print('============================================================================') ### SRC - Do you need these print statements? could you have used the input below
     print('NEW ROUND: Enter rock, paper or scissors. Enter finish to end the game.')
-    userinput = str(input())
-    j = randint(0,2)
+    userinput = str(input()) ### SRC - do you need str here?
+    j = randint(0,2)  ### SRC - meaningful variable names please!!!
     if userinput in choice:
         i = choice.index(userinput)
         if i == j:
@@ -19,7 +23,7 @@ while userinput != -1:
             score()
         elif (i == 0 and j == 1) or (i == 1 and j == 2) or (i == 2 and j == 0):
             Cscore += 1
-            result('lose')
+            result('lose') 
         elif (i == 0 and j == 2) or (i == 1 and j == 0) or (i == 2 and j == 1):
             Uscore += 1
             result('win')
