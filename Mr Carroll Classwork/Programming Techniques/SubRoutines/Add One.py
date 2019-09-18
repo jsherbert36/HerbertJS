cda4b2a4b4e23919ec68eb1c9cdf9811bc78b188
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-09-18 12:57:15 +0100

    Create Add One.py

---
+++ Mr Carroll Classwork/Programming Techniques/SubRoutines/Add One.py
@@ -0,0 +1,21 @@
+num,name = 3,"Dave"
+name_list = [name[int(i)] for i in range(len(name))]
+
+def add_one(number):
+    return number + 1
+#end function
+
+def add_s(name):
+    return name + "s"
+#end function
+
+def append_s(name):
+    name.append("s")
+    print (name)
+#end procedure
+
+print(add_s(name))
+print(name)
+
+append_s(name_list)
+print(name_list)
