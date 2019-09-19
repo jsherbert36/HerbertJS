76a6e85be8b961947ea4bde953750311f9aea4c8
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-09-19 09:42:50 +0100

    Update Fibonacci.py

--- Mr Carroll Classwork/Programming Techniques/SubRoutines/Fibonacci.py
+++ Mr Carroll Classwork/Programming Techniques/SubRoutines/Fibonacci.py
@@ -4,17 +4,16 @@ def fib1(n):
         return 1
     else:
         return fib1(n - 2) + fib1(n - 1)
+    #end if
 #end function
-my_list = []
-def fib2(n):
-    global my_list
+def fib2(n,my_list):
     my_list.append(1)
     my_list.append(1)
     for i in range(2,n):
         my_list.append((my_list[i-1] + my_list[i-2]))
-    #next
-    [print(i,end=' ') for i in my_list]   
+    #next  
 #end procedure
-
+List1 = []
 print(fib1(Num))
-fib2(Num)
+fib2(Num,List1)
+[print(i,end=' ') for i in List1]
