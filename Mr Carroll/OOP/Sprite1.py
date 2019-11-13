84dffd4b5a2f62ff83d7a608bced29af093ad7ee
jsherbert36 <54801612+jsherbert36@users.noreply.github.com>
2019-11-13 11:52:35 +0000

    add snow

--- Mr Carroll/OOP/Sprite1.py
+++ Mr Carroll/OOP/Sprite1.py
@@ -7,13 +7,12 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
- 
 class Block(pygame.sprite.Sprite):
     """
     This class represents the ball
     It derives from the "Sprite" class in Pygame
     """
-    def __init__(self, color, width, height):
+    def __init__(self):
         """ Constructor. Pass in the color of the block,
         and its x and y position. """
         # Call the parent class (Sprite) constructor
@@ -21,8 +20,9 @@ class Block(pygame.sprite.Sprite):
  
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = pygame.image.load("Snowflake.jpg").convert()
+        self.image = pygame.transform.smoothscale(self.image, (30,30))
+        self.image.set_colorkey(BLACK)
  
         # Fetch the rectangle object that has the dimensions of the image
         # image.
@@ -41,19 +41,22 @@ class Block(pygame.sprite.Sprite):
         # Move block down one pixel
         self.rect.y += 1
         # If block is too far down, reset to top of screen.
-        if self.rect.y > 410:
+        if self.rect.y > size[1] + 10:
             self.reset_pos()
  
 class Player(Block):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([20, 20])
+        self.image.fill(WHITE)
     def update(self):
-        
         pos = pygame.mouse.get_pos()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
  
 # Initialize Pygame
 pygame.init()
-size = (700,400)
+size = (700,500)
 screen = pygame.display.set_mode(size)
  
 # This is a list of 'sprites.' Each block in the program is
@@ -62,35 +65,35 @@ block_list = pygame.sprite.Group()
 # This is a list of every sprite. All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
  
-for i in range(50):
-    block = Block(WHITE, 20, 20)
-    block.rect.x = random.randrange(size[0])
+for i in range(60):
+    block = Block()
+    block.rect.x = random.randrange((size[0]-30))
     block.rect.y = random.randrange(size[1])
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
  
-player = Player(RED, 30, 30)
+player = Player()
 all_sprites_list.add(player)
-done = False
+game_over = False
 clock = pygame.time.Clock()
 score = 0
-font = pygame.font.Font('freesansbold.ttf', 40)
+font = pygame.font.Font('freesansbold.ttf', 30)
 
-# -------- Main Program Loop -----------
-while not done:
+# -------- Main Program Loop ----------- #
+while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
- 
+            game_over = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_over = True
     screen.fill(BLACK)
-    # Calls update() method on every sprite in the list
     all_sprites_list.update()
     # See if the player block has collided with anything.
     blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
     for block in blocks_hit_list:
         score += 1
-        # Reset block to the top of the screen to fall again.
         block.reset_pos()
 
     # Draw all the sprites
